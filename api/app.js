761dd82abf475add1c9be0a5583c4522e4134ee6
Starling feliz <starling2802@gmail.com>
2026-09-09 14:17:45 -0400

    fix(cors): permite frontend docker

--- api/app.js
+++ api/app.js
@@ -27,7 +27,9 @@ app.use(cors({
     "http://127.0.0.1:3000",
     "http://localhost:3000",
     "http://127.0.0.1:5500",
-    "http://localhost:5500"
+    "http://localhost:5500",
+    "http://10.0.0.84:8082",
+    "http://grade.home"
   ]
 }));
 
